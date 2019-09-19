21eb63f2ae688fc4b8343c02253e89aca4d998c6
mikearaya <mikaelaraya12@gmail.com>
2019-09-19 20:14:30 +0300

    added compiler

---
+++ contracts/Inbox.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.11;
+
+contract Inbox {
+    
+    string public message;
+    
+     constructor(string memory initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+    
+
+} 
